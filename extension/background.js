9b95a3cdb0c61b5986da1463b514e1dd2007f779
halvo <jordan@vrasa.com>
2025-12-16 19:13:58 -0500

    intial build

---
+++ extension/background.js
@@ -0,0 +1,10 @@
+/**
+ * Mute - Background Script
+ * Manages native host connection and message routing
+ */
+
+(function() {
+    'use strict';
+
+    console.log('[Mute] Background script initialized');
+})();
---
+++ extension/content.js
@@ -0,0 +1,10 @@
+/**
+ * Mute - Content Script
+ * Runs on Twitch pages to detect ad playback
+ */
+
+(function() {
+    'use strict';
+
+    console.log('[Mute] Content script loaded on:', window.location.href);
+})();
